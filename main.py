fab533bf4d39de9d0b44b7f49f5aa02edca1eecd
Marc <mcavigli@gmail.com>
2022-06-03 23:52:06 -0700

    First push of module

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import shutil
+'''Creates a folder structure for a year with 12 folders inside for months'''
+
+
+def main():
+    years_dir = set()
+    file_types = ['.jpg', '.png', '.mov', 'jpeg']
+
+    for i in os.listdir('.'):
+        if i[-3:] == '.py':
+            continue
+        elif i[-4:] in file_types:
+            year = i[:4]
+            month = i[5:7]
+            year_month = year + '/' + month
+            if year not in years_dir:
+                years_dir.add(year)
+                os.mkdir(year)
+            if not os.path.isdir(year_month):
+                os.mkdir(year_month)
+            src = './' + i
+            final_path = year_month + '/' + i
+            shutil.move(src, final_path)
+
+
+if __name__ == '__main__':
+    main()
